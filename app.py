a7648bf97b99324351f98949675a36610b7c5f0e
gouri <gourivpawar@gmail.com>
2023-09-01 15:41:47 +0530

    debug4 cdn version updatedm

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ app.config['SECRET_KEY'] = "secret key"
 db = SQLAlchemy(app)
 app.secret_key = "sfdjkafnk"
 socketio = SocketIO(app,cors_allowed_origins="https://careforyou.onrender.com")
-cors = CORS(app, resource={r"/*": {"origins": "*"}})
+#cors = CORS(app, resource={r"/*": {"origins": "*"}})
 #CORS(app, resources={r"/*": {"origins": "https://careforyou.onrender.com"}})
 app.config['CORS_HEADERS'] = 'Content-Type'
 
