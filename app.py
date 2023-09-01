6a03c60dcc703568b9866eee5a7ff8ff3fce2cb1
gouri <gourivpawar@gmail.com>
2023-09-01 15:52:07 +0530

    debug4 removed cors layer function

--- app.py
+++ app.py
@@ -940,23 +940,6 @@ def handle_send_message_event(data):
 
 
 
-@app.after_request
-def after_request_func(response):
-    origin = request.headers.get('Origin')
-    if request.method =='OPTIONS':
-        response = make_response()
-        response.headers.add('Access-Control-Allow-Credentials','true')
-        response.headers.add('Access-Control-Allow-Headers','Content-Type')
-        response.headers.add('Access-Control-Allow-Headers','x-csrf-token')
-        response.headers.add('Access_Control-Allow-Methods','GET,POST,OPTIONS,PUT,PATCH.DELETE')
-        if origin:
-            response.headers.add('Access-Control-Allow-Origin',origin)
-    else:
-        response.headers.add('Access-Control-Allow-Credentials', 'true')
-        response.headers.add('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-        if origin:
-            response.headers.add('Access-Control-Allow-Origin', origin)
-    return response
 
 
 
