5c857e55c1492bb7f7e2ad4f32beaed784674ef9
gouri <gourivpawar@gmail.com>
2023-09-09 23:24:43 +0530

    updated app.py and view 9

--- app.py
+++ app.py
@@ -968,26 +968,6 @@ def get_google_provider_cfg():
     return requests.get(GOOGLE_DISCOVERY_URL).json()
 
 
-@app.after_request
-def after_request_func(response):
-    origin = request.headers.get('Origin')
-    if request.method =='OPTIONS':
-        response = make_response()
-        response.headers.add('Access-Control-Allow-Credentials','true')
-        response.headers.add('Access-Control-Allow-Headers','Content-Type')
-        response.headers.add('Access-Control-Allow-Headers','x-csrf-token')
-        response.headers.add('Access_Control-Allow-Methods','GET,POST,OPTIONS,PUT,PATCH.DELETE')
-        if origin:
-            response.headers.add('Access-Control-Allow-Origin',origin)
-    else:
-        response.headers.add('Access-Control-Allow-Credentials','true')
-        if origin:
-            response.headers.add('Access-Control-Allow-Origin',origin)
-    return response
-
-
-
-
 if __name__ == '__main__':
     #eventlet.monkey_patch()
     socketio.run(app, host='0.0.0.0', debug=True)
